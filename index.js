4ff6c6077cbc8fb7f40f88e16d28edadaf56f15d
liamca1 <88624211+liamca1@users.noreply.github.com>
2022-04-09 19:23:47 +0200

    endpoint URL's added/updated

--- index.js
+++ index.js
@@ -5,9 +5,80 @@ const app = express();
 
 app.use(morgan('common'));
 
-// Express GET route with the endpoint "/movies" that returns a JSON object.
+let movies = {
+    
+}
+
+// Return a list of all movies to the user
 app.get('/movies', (req, res) => {
-    res.json(topMovies);
+    res.json(movies);
+});
+
+// Return data about a single movie by title
+app.get('/movies/:title', (req, res) => {
+    res.json(movies.find((movies) =>
+    { return movies.title === req.params.title}));
+});
+
+// Return description (data) about a genre by name
+app.get('/movies/genres/:genre', (req, res) => {
+    res.json(genres.find((genres) =>
+    { return genres.genre.description === req.params.genre }));
+});
+
+// Return data about a director by name
+app.get('/movies/directors/:director', (req, res) => {
+    res.json(directors.find((directors) =>
+    { return directors.director === req.params.director}));
+});
+
+// Allow new users to register
+app.post('/users', (req, res) => {
+    let newUser = req.body;
+
+    if (!newUser.userName) {
+        const message = 'Please create your username';
+        res.status(400).send(message);
+    }   else {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).send(newUser);
+    }
+});
+
+// Allow users to update their user info (username)
+app.put('/users/:usernanme', (req, res) => {
+    let username = users.find((username) => { return username.username === req.params.name });
+
+    if (username) {
+        username.username[req.params.username] = parseInt(req.params.username);
+        res.status(201).send('User' + ' successfully updated their username to ' + req.params.username + '.');
+    }   else {
+        res.status(404).send('User with the username ' + req.params,username + ' was not found.');
+    }
+});
+
+// Allow users to add a movie to their list of favourites
+
+
+// Allow users to delete a movie from their list of favourites
+app.delete('/users/:username/favourites/:title', (req, res) => {
+    let title = users.find((title) => { return users.username === req.params.username });
+
+    if (title) {
+        users = users.filter((obj) => { return obj.username !== req.params.username });
+        res.status(201).send('User ' + req.params.username + ' has been deleted.');
+    }
+});
+
+// Allow existing users to deregister
+app.delete('/users/:username', (req, res) => {
+    let user = users.find((user) => { return user.username === req.params.username });
+
+    if (user) {
+        users = users.filter((obj) => { return obj.username !== req.params.username });
+        res.status(201).send('User: ' + req.params.username + ' was deleted.');
+    }
 });
 
 // Express GET route with the endpoint "/" that returns a default textual response. 
