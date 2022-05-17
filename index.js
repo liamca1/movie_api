34e3029c7ced465b9160c7ebbd8e0a15ef485ff3
liamca1 <88624211+liamca1@users.noreply.github.com>
2022-05-17 10:02:27 +0200

    Update

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ mongoose.connect('mongodb://localhost:27017/newFlixDB', {
  });
 */
 
+
 mongoose.connect(process.env.CONNECTION_URI || "mongodb://localhost:27017/newFlixDB", { useNewUrlParser: true, useUnifiedTopology: true, });
 
 const  morgan =require('morgan'),
